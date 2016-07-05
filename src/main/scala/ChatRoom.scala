78e4618f19c477d5caf6b5ae2a2fd948cbcf9af1
iliao <iliao@digitalriver.com>
2016-07-05 19:24:49 +0800

    support browsers to talk with each other

--- src/main/scala/ChatRoom.scala
+++ src/main/scala/ChatRoom.scala
@@ -1,67 +1,38 @@
-import ChatRoom.ChatMessage
+import ChatRoom.{CachedChatMessage, ChatMessage, GetChatMessages}
 import akka.actor.{Actor, ActorRef, Props}
-import akka.io.Tcp._
-import akka.util.ByteString
+
+import scala.collection.mutable
 
 object ChatRoom {
   val PATH = "chat-room"
-  case class ChatMessage(from:String, message:String)
+  case class ChatMessage(seqId:Long, message:String)
+  case class CachedChatMessage(seqId:Long, message:String)
+  case class GetChatMessages(lastMsgId:Long)
 }
 
-/** [[ChatRoom]] will create [[UserSession]] when receiving new connection and forward [[ChatMessage]] to them */
 class ChatRoom extends Actor {
 
+  var seqId = 0;
+  var cachedChatMessages = new mutable.Queue[CachedChatMessage]
+
   override def receive: Receive = {
-    case Connected(remote, local) =>
-      println("new connnection. remote:" + remote + " local:" + local)
-      val connection = sender()
-      val userSession = context.actorOf(Props(classOf[UserSession],connection))
-      connection ! Register(userSession)
-    case chatMessage @ ChatMessage(name:String, message:String) =>
-      println("chatRoom:" + chatMessage)
-      context.children.foreach(_ forward chatMessage)
-    case msg => println("chatroom receive unexpected message:" + msg)
+    case GetChatMessages(lastMsgId:Long) =>
+      var result = ""
+      cachedChatMessages.filter(_.seqId > lastMsgId).foreach(result += _.message + "\n")
+      result = seqId + ":" + result
+      sender() ! result
+    case chatMessage @ ChatMessage(userOwnLastSeqId:Long, message:String) =>
+      seqId += 1
+      cachedChatMessages = cachedChatMessages :+ new CachedChatMessage(seqId, message)
+      dequeueMessageFromQueueToFixedSize
+  }
+
+  private def dequeueMessageFromQueueToFixedSize(): Unit = {
+    while (cachedChatMessages.size > 5) {
+      println("drop old message:" + cachedChatMessages.dequeue)
+    }
   }
 
 }
 
-/**
-  * [[UserSession]] will buffer received data from tcp connection and send [[ChatMessage]] to [[ChatRoom]].
-  * [[UserSession]] will write message to remote client when receiving a [[ChatMessage]]
-  * [[UserSession]] will close tcp connection and stop itself when receiving a "bye" command from remote client
-  */
-class UserSession(connection:ActorRef) extends Actor {
-  import akka.io.Tcp.{PeerClosed, Received, Write}
 
-  // FIXME should be a fixed size buffer, prevent out of memory problem
-  var buffer = ""
-
-  override def receive: Receive = {
-    case ChatMessage(name:String, message:String) =>
-      println("user received:" + message + ", connection:" + connection)
-      connection ! Write(ByteString("\r\n" + message.trim))
-    case Received(data) => {
-      buffer += data.utf8String
-      val isNewLine: Boolean = data.utf8String == "\n" || data.utf8String == "\r\n"
-      if (isNewLine) {
-        processBuffer()
-      }
-    }
-    case PeerClosed => {
-      println("connection closed")
-      context stop self
-    }
-  }
-
-  private def processBuffer(): Unit = {
-    if ("bye" == buffer.trim.toLowerCase) {
-      println("receive bye")
-      sender() ! Close
-      context stop self
-    } else {
-      context.parent ! ChatMessage(self.path.name, buffer)
-      buffer = ""
-    }
-  }
-
-}
